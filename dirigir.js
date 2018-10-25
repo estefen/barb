4d11db50ee6ea02a856554bba54075ef4de7de3a
estefen <44439672+estefen@users.noreply.github.com>
2018-10-25 09:19:28 -0300

    Add files via upload

---
+++ dirigir.js
@@ -0,0 +1,46 @@
+//var idade =prompt("Qual sua idade?");
+//if( idade == "x"){
+  //  alert("Você sabe a idade correta para dirigir?");
+//}else{
+    
+//var motorista = prompt ("Você pode dirigir?");
+//if (motorista == "17"){
+  //  alert ("Que pena você só pode dirigir se já tiveres alcançado a maior idade penal de 18 anos");
+//} else if (motorista = "18");
+//    alert (" Legal! Você já tem idade para ser um grande motorista");
+//}else{
+
+
+
+
+//var contadorFerias= 5;
+
+//while(contadorFerias > 0){
+
+//    alert("Faltam " + contadorFerias + " dias!");
+
+  //  contadorFerias -=1;
+//}
+  //  alert ("Boas Férias!!");
+
+
+
+
+
+
+//var tempo= prompt ("QUanto tempo falta?");
+//while(tempo > 0){
+//
+  //  alert("Fatam " + tempo + " segundos");
+//
+  //  tempo -= 1;
+//} 
+//    alert (" Seja Feliz!!!");
+
+
+var contador = 10;
+while (contador >0){
+    alert ("Falta + contador + " segundos"):
+    contador -= 2;
+}
+alert ("Parabéns!"):
